d6a028debdfa19417d9b23637a84795d4c5fff07
Vladimir <v.lelicanin@dooapp.net>
2016-12-20 20:06:08 +0100

    lesson3

--- lession3/arrays2.php
+++ lession3/arrays2.php
@@ -1,6 +1,9 @@
 <?php
-//implode, explode, in_array, array_map, array_filter
-//array_push, array_pop, array_shift, array_unshift, array_splice
+/*
+ * implode, explode, in_array, array_map, array_filter
+ * array_push, array_pop, array_shift, array_unshift, array_splice
+ */
+
 
 $automobili = ['mazda', 'honda', 'toyota', 'ford', 'mercedes', 'lexus'];
 $string_automobili = "mazda, honda, toyota";
@@ -14,7 +17,8 @@ $user = [
     'lastname' => 'Smith'
 ];
 
-$novi_array = array_map(function($el){
+$novi_array = array_map(function ($el)
+{
     if ($el == "mazda")
     {
         return $el . "bla";
@@ -24,15 +28,19 @@ $novi_array = array_map(function($el){
 }, $automobili);
 
 
-$novi_array = array_filter($automobili, function($el){
-    if($el) return true;
+$novi_array = array_filter($automobili, function ($el)
+{
+    if ($el) return true;
+
     return false;
 });
 
 
-$novi_array = array_filter($automobili, function($el){
-    if ( strlen($el) == 5) return true;
+$novi_array = array_filter($automobili, function ($el)
+{
+    if (strlen($el) == 5) return true;
+
     return false;
 });
 
-var_dump( $novi_array);
+var_dump($novi_array);
